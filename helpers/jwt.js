403ca7f7b8a9a62788c786bac8aa5711f631478b
Day Wa <73962589+DIGITALDAYWA@users.noreply.github.com>
2022-05-30 14:31:11 +0630

    Update jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -19,10 +19,10 @@ function authjwt() {
     isRevoked: isRevoked,
   }).unless({
     path: [
-      { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
       { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
       { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-      // { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS'] },
       `${api}/users/login`,
       `${api}/users/register`,
     ],
